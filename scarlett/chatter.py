454d6787305fcb527946827a5418b70c5bf0a158
Byron <37745048+byhowe@users.noreply.github.com>
2020-04-01 07:32:41 -0500

    Send all messages instead of last 50

--- scarlett/chatter.py
+++ scarlett/chatter.py
@@ -237,7 +237,7 @@ def get_messages(conn: Connection, result: Result):
         db: Database = current_conn.db
         for message in db.get_collection("messages").find({
             "squad": squad_id
-        }).limit(50).sort("timestamp"):
+        }).sort("timestamp"):
             posts.append({
                 "from": message["from"],
                 "timestamp": str(message["timestamp"]),
