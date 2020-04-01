93e5c2e6aa22033f00baefc1d56f17e09a10d00b
Byron <37745048+byhowe@users.noreply.github.com>
2020-04-01 16:47:22 -0500

    Server sends back last 70 messages instead of all

--- scarlett/chatter.py
+++ scarlett/chatter.py
@@ -238,7 +238,7 @@ def get_messages(conn: Connection, result: Result):
         db: Database = current_conn.db
         for message in db.get_collection("messages").find({
             "squad": squad_id
-        }).sort("timestamp"):
+        }).sort("timestamp").limit(70):
             posts.append({
                 "from": message["from"],
                 "timestamp": str(message["timestamp"]),
